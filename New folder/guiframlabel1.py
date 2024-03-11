982bd7ac2c7cbbb60264ab37587397835eb7d7a2
ajaydsda <162972970+ajaydsda@users.noreply.github.com>
2024-03-11 14:39:22 +0530

    Add files via upload

---
+++ New folder/guiframlabel1.py
@@ -0,0 +1,8 @@
+import tkinter 
+window = tkinter.Tk()
+frame=tkinter.Frame(window)
+
+frame.pack()
+label = tkinter.Label(frame,text="Hello FYDSDA" )
+label.pack()
+window.mainloop()
---
+++ New folder/guilabel.py
@@ -0,0 +1,24 @@
+import tkinter 
+window = tkinter.Tk()
+
+label = tkinter.Label(window,text="FYDSDA CLASS", fg="blue", bg="white",height="2",bd="5",justify='left')
+label.pack()
+label.config(text="FYDSDA")
+
+label2 = tkinter.Label(window,text="Welcomer")
+
+label2.pack()
+
+window.mainloop()
+
+##
+##import tkinter 
+##window = tkinter.Tk()
+##data==tkinter.StringVar()
+##data.set('Data to display')
+##label = tkinter.Label(window,textvariable=data)
+##label.pack()
+##window.mainloop()
+
+
+
